95c093ad8e0ba8384a1cfd05b2970c752c4bcf61
davidmaciuca <davidmaciuca@localhost>
2024-11-13 15:33:05 -0600

    Done with this shit...

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -1,38 +1,32 @@
 package studio8;
 
 public class MultipleChoiceQuestion extends Question {
-	
+
+	private String[] choices;
+
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
-		//FIXME
-		
-		//Call the super class constructor, then create and set
-		//instance variables for any values that aren't handled
-		//by the base class
+		// Call the super class constructor for common fields
+		super(prompt, answer, points);
+		// Set the unique field for multiple-choice options
+		this.choices = choices;
 	}
-	
+
 	public void displayPrompt() {
-		//FIXME
-		
-		/*
-		 * Use the method from the base class to display the prompt,
-		 * Then write more code to display all of the choices like so:
-		 * 
-		 * 1. Choice1
-		 * 2. Choice2
-		 * 3. Choice3
-		 * ...
-		 * N. ChoiceN
-		 */
-		//
+		// Display the prompt using the base class method
+		super.displayPrompt();
+		// Display each choice with a number prefix
+		for (int i = 0; i < choices.length; i++) {
+			System.out.println((i + 1) + ". " + choices[i]);
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		String[] choices = {"seven", "nine", "eight", "six"};
+		String[] choices = { "seven", "nine", "eight", "six" };
 		Question multipleChoice = new MultipleChoiceQuestion("What studio is this?", "3", 1, choices);
 		multipleChoice.displayPrompt();
-		System.out.println(multipleChoice.checkAnswer("hi"));//wrong
-		System.out.println(multipleChoice.checkAnswer("1"));//wrong
-		System.out.println(multipleChoice.checkAnswer("3"));//right
+		System.out.println(multipleChoice.checkAnswer("hi"));// wrong
+		System.out.println(multipleChoice.checkAnswer("1"));// wrong
+		System.out.println(multipleChoice.checkAnswer("3"));// right
 	}
 
 }
--- src/studio8/Quiz.java
+++ src/studio8/Quiz.java
@@ -5,41 +5,53 @@ import java.util.Scanner;
 public class Quiz {
 
 	private Question[] questions;
-	
+
 	public Quiz(Question[] questions) {
 		this.questions = questions;
 	}
-	
+
 	private String getUserAnswer() {
-		Scanner in = new Scanner(System.in);
-		System.out.print("Please enter your answer: ");
-		String out = in.next();
-		return out;
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.print("Please enter your answer: ");
+			String out = in.next();
+			return out;
+		}
 	}
-	
+
 	public int getTotalPoints() {
 		int sum = 0;
-		for(Question q : questions) {
+		for (Question q : questions) {
 			sum += q.getPoints();
 		}
 		return sum;
 	}
-	
+
 	public void takeQuiz() {
-		//FIXME
+		int totalEarnedPoints = 0;
+		for (Question q : questions) {
+			q.displayPrompt();
+			String userAnswer = getUserAnswer();
+			int pointsEarned = q.checkAnswer(userAnswer);
+			totalEarnedPoints += pointsEarned;
+			System.out.println("Points earned for this question: " + pointsEarned + "\n");
+		}
+		int totalPoints = getTotalPoints();
+		System.out.println("Quiz completed!");
+		System.out.println("Total points earned: " + totalEarnedPoints + " out of " + totalPoints);
 	}
-	
+
 	public static void main(String[] args) {
 		Question q = new Question("What number studio is this?", "8", 2);
 
-		String[] choices = {"seven", "nine", "eight", "six"};
+		String[] choices = { "seven", "nine", "eight", "six" };
 		Question multipleChoice = new MultipleChoiceQuestion("What studio is this?", "3", 1, choices);
 
-		choices = new String[] {"instance variables", "git", "methods", "eclipse"};
-		Question selectAll = new SelectAllQuestion("Select all of the following that can be found within a class:", "13", choices);
+		choices = new String[] { "instance variables", "git", "methods", "eclipse" };
+		Question selectAll = new SelectAllQuestion("Select all of the following that can be found within a class:",
+				"13", choices);
 
-		Question[] questions = {q, multipleChoice, selectAll}; //create and add more questions!
-		
+		Question[] questions = { q, multipleChoice, selectAll }; // create and add more questions!
+
 		Quiz studio8quiz = new Quiz(questions);
 		studio8quiz.takeQuiz();
 	}
